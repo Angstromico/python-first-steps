2036898ba35550461fc3f0af7232496ddb387b6b
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-11-06 11:39:52 -0400

    feat: Capitalize name

--- hello.py
+++ hello.py
@@ -3,6 +3,9 @@ name = input("What's your name? ")
 #Remove white spaces from str
 name = name.strip()
 
+#Capitalize the first letter
+name = name.capitalize()
+
 #print("Hello", name)
 print(f"Hello, {name}")
 
