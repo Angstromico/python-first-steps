7438ce9dee84b7dceda28363de73ccbd77f0f831
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-11-06 13:29:53 -0400

    feat: Split name

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 name = input("What's your name? ").strip().title()
+parts = name.split()
 
 #Remove white spaces from str and capitalize
 #name = name.strip().title()
@@ -7,7 +8,11 @@ name = input("What's your name? ").strip().title()
 #name = name.capitalize()
 #name = name.title()
 
+#Split user name into main name and lastname
+first_name = name.split(" ")[0]
+last_name = parts[1] if len(parts) > 1 else ""
+
 #print("Hello", name)
-print(f"Hello, {name}")
+print(f"Hello, {first_name} {last_name}")
 
 # This my first step into Python
