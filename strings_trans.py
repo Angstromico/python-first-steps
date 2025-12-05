62c02e10881f6cab25329322b4fccf2657e23759
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-12-05 17:37:01 -0400

    feat: Strings transformations

---
+++ strings_trans.py
@@ -0,0 +1,12 @@
+def main(): 
+    string_base = "Hello, World!"
+    uppercase_string = string_base.upper()
+    lowercase_string = string_base.lower()
+
+    print("Lowercase:", string_base)
+    print("Uppercase:", uppercase_string)
+    print("Lowercase:", lowercase_string)
+
+
+if __name__ == "__main__":
+    main()
