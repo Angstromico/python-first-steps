2d696bc8d7402a2df97b2b9ad8c621c78623b73c
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-12-06 17:28:39 -0400

    feat: Capitalize on strings_trans

--- strings_trans.py
+++ strings_trans.py
@@ -2,10 +2,12 @@ def main():
     string_base = "Hello, World!"
     uppercase_string = string_base.upper()
     lowercase_string = string_base.lower()
+    capitalized_string = lowercase_string.capitalize()
 
     print("Lowercase:", string_base)
     print("Uppercase:", uppercase_string)
     print("Lowercase:", lowercase_string)
+    print("Capitalized:", capitalized_string)
 
 
 if __name__ == "__main__":
