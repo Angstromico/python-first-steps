acb80b0fb558c5b3170f25630106d7cde6cef2af
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-01-03 00:19:40 -0400

    feat: Implement main function to generate a random ID based on user inputs

---
+++ random_id.py
@@ -0,0 +1,14 @@
+def main(): 
+    # Generate random ID with the name, lastname date and four random digits from user inputs
+    from random import randint
+
+    name = input("Enter your name: ").strip().title()
+    lastname = input("Enter your lastname: ").strip().title()
+    date = input("Enter the year of birth: ").strip()
+    last_date_two_digits = date[-2:] if len(date) >= 2 else date.zfill(2)
+    random_digits = f"{randint(0, 9999):04d}" # Four random digits with leading zeros
+    random_id = f"{name[:2]}{lastname[:2]}{last_date_two_digits}{random_digits}"
+    print(f"Your random ID is: {random_id}")
+
+if __name__ == "__main__":
+    main()
