1dc2c03217d4721bf1939af12709e98cb444c5fc
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-02-19 00:17:52 -0400

    feat: Update CSV reading to include country and modify students data

--- names/csv_names.py
+++ names/csv_names.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 def main(): 
     students = []
@@ -6,13 +7,16 @@ def main():
     file_path = os.path.join(script_dir, "students.csv")
 
     with open(file_path, "r") as file:
-        for line in file:
-            name, house = line.strip().split(",")
-            student = {"name": name, "house": house}
-            students.append(student)
+        reader = csv.DictReader(file, fieldnames=["name", "home", "country"])
+        next(reader)
+        for row in reader:
+            students.append({"name": row["name"], "home": row["home"], "country": row["country"]})
         
         for student in sorted(students, key=lambda s: s["name"]):
-            print(f"{student['name']} from {student['house']}")
+            if student["country"]:
+                print(f"{student['name']} from {student['home']} in {student['country']}")
+            else:
+                print(f"{student['name']} from {student['home']}")
 
 
 if __name__ == "__main__":
